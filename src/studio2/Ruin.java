cadd2de0b2bda02aca5c29a3a89a15a327077c62
Audrey Chiang <audrey3chiang@gmail.com>
2023-09-14 10:37:40 -0500

    gambler's ruin

--- src/studio2/Ruin.java
+++ src/studio2/Ruin.java
@@ -6,33 +6,46 @@ public class Ruin {
 
 	public static void main(String[] args) {
 		Scanner in = new Scanner(System.in);
-		System.out.println("Start amount: ");
+		System.out.print("Start amount: ");
 		double startAmount0 = in.nextDouble();
-		System.out.println("Win chance: ");
+		System.out.print("Win chance: ");
 		double winChance0 = in.nextDouble();
-		System.out.println("Win limit: ");
+		System.out.print("Win limit: ");
 		double winLimit0 = in.nextDouble();
-		System.out.println("Total simulations: ");
-		double nSims = in.nextDouble();
+		System.out.print("Total simulations: ");
+		int nSims = in.nextInt();
 		
 		int wins = 0;
 		int losses = 0;
 
 		for (int i = 0; i < nSims; i++) {
+			String result;
 			double startAmount = startAmount0;
 			double winChance = winChance0;
 			double winLimit = winLimit0;
 			
 			while (startAmount > 0 && startAmount < winLimit) {
 				startAmount = simulant(startAmount, winChance, winLimit);
-				System.out.println(startAmount);
 			}
 			if (startAmount >= winLimit) {
 				wins++;
+				result = "WIN";
+				
 			} else {
 				losses++;
+				result = "LOSE";
 			}
+			
+			System.out.println("Simulation " + (i + 1) + ": " + result);
 		}
+		double alpha = (1-winChance0) / winChance0;
+		double expectedRuin = (Math.pow(alpha, startAmount0) - Math.pow(alpha, winLimit0)) / (1-Math.pow(alpha, winLimit0));
+		
+		System.out.println("Losses: " + losses);
+		System.out.println("Simulations: " + nSims);
+		System.out.println("Ruin rate from simulation: " + (double)losses/nSims);
+		System.out.println("Expected ruin rate: " + expectedRuin);
+		
 
 	}
 
